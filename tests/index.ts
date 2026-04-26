b6eeb7549d21670b9a4b79a5c55543b42fede560
Upendar Bathula <upendarb1999@gmail.com>
2026-04-26 13:11:49 +0800

    I am pushing all files, 1st push

---
+++ tests/index.ts
@@ -0,0 +1,9 @@
+class StudentDetails {
+    name: string = "TetsUser";
+    constructor() {
+        console.log("Student Name : " + this.name);
+    }
+}
+
+// Create an object instance
+const student = new StudentDetails();
